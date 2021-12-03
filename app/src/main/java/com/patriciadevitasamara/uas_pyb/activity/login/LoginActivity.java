ca34f6b9007fb436226e752964c934ddb73231d5
patriciads17 <patriciadevitas30@gmail.com>
2021-12-03 12:58:00 +0800

    coba

---
+++ app/src/main/java/com/patriciadevitasamara/uas_pyb/activity/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.patriciadevitasamara.uas_pyb.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
--- app/src/main/java/com/patriciadevitasamara/uas_pyb/api/RetrofitClient.java
+++ app/src/main/java/com/patriciadevitasamara/uas_pyb/api/RetrofitClient.java
@@ -1,4 +1,17 @@
 package com.patriciadevitasamara.uas_pyb.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
-}
+    public static final String BASE_URL = "https://pbp-web-api.herokuapp.com/api/";
+    public static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
